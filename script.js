8ef3e24f69d9c8c7f2d91e91239b1641f04b62d6
reng <67654603+engrebecca@users.noreply.github.com>
2020-09-21 20:49:18 -0700

    Add variables for DOM selectors

---
+++ script.js
@@ -0,0 +1,42 @@
+// Select DOM elements
+// Start quiz card
+var start = document.getElementById("start");
+// Question cards
+var q1 = document.getElementById("question1");
+var q2 = document.getElementById("question2");
+var q3 = document.getElementById("question3");
+var q4 = document.getElementById("question4");
+var q5 = document.getElementById("question5");
+var question = document.getElementById("question");
+var optionA = document.getElementById("optionA");
+var optionB = document.getElementById("optionB");
+var optionC = document.getElementById("optionC");
+var optionD = document.getElementById("optionD");
+// Enter initials card
+var scoreInitial = document.getElementById("scoreInitial");
+var score = document.getElementById("score");
+var nameSubmit = document.getElementById("nameSubmit");
+var name = document.querySelector("input");
+// High score card
+var highScore = document.getElementById("highScore");
+var goBack = document.getElementById("goBack");
+var clearScore = document.getElementById("clearScore");
+
+// console.log(start);
+// console.log(q1);
+// console.log(q2);
+// console.log(q3);
+// console.log(q4);
+// console.log(q5);
+// console.log(question);
+// console.log(optionA);
+// console.log(optionB);
+// console.log(optionC);
+// console.log(optionD);
+// console.log(scoreInitial);
+// console.log(score);
+// console.log(nameSubmit);
+// console.log(highScore);
+// console.log(goBack);
+// console.log(clearScore);
+console.log(name);
